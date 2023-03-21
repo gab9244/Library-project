5290ae93d8c90a53ce346b20969eb17976d65394
gab9244 <gab9244@gmail.com>
2023-03-21 09:54:58 -0300

    Almost done

--- index.js
+++ index.js
@@ -1,113 +1,101 @@
-let myLibrary = []
- 
-
-
-
-    function Book(title, author, pages,read,isread,remove){
-    this.bookId = `book${++Book.id}`
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.isread= isread
-    this.remove = remove
-   
+let myLibrary =[]
+let Title = document.getElementById('title')
+let Author = document.getElementById('author')
+let Pages = document.getElementById('pages')
+ function Books (Title, Author, Pages,Read){
+        this.Title = Title
+        this.Author = Author
+        this.Pages = Pages
+        this.Read = Read
+        
     }
+
+
+
+// Taking the DOM elements
+let submitBtn = document.getElementById('submitBtn')
+let form = document.getElementById('card')
+let buttons = document.querySelectorAll('button')
+let formContainer = document.getElementById('formContainer')
+let checkbox = document.getElementById('checkbox')
+let inputs = document.querySelectorAll('input').value
 
  function render(){
-   cardsSpace.innerHTML =''
-   
+    let cardsSpace = document.getElementById('cards-space')
+        cardsSpace.innerHTML =''
+    for(let i = 0; i<myLibrary.length;i++){
+    let book = myLibrary[i]
+    let cards = document.createElement('div')
+    cards.classList.add('cards')
+    cards.innerHTML = `<p>${book.Title}</p>
+                        <p>${book.Author}</p>
+                        <p>${book.Pages}</p>
+                        <button class ='removeBtn' onclick ='removeBook(${i})'>Remove</button>`
+                        cards.append(book.Read)
  
-    for(let i = 0; i< myLibrary.length;i++)
-    {
-       
-        let cards = document.createElement('div')
-        
-        
-        cards.classList.add('cards')
-       
-        cards.setAttribute('id', `${i}`)
-        cardsSpace.append(cards)
-        let book = myLibrary[i]
-        
-      cards.innerHTML = `
-      <p>${book.title}</p>
-      <p>${book.author}</p>
-      <p>${book.pages}</p>`
-      cards.append(book.read)
-      cards.append(book.remove)
-      
-      
-       
-    }
-    
- }
-function addBooksToLibrary(){
+                      
+                       
+                        
+    cardsSpace.append(cards)
+   
+  
+   
+}
 
+ }
+
+ 
+ 
+
+ function removeBook(index){
+    myLibrary.splice(index,1)
+    render()
+ }
+function addBookToLibrary(){
     let Title = document.getElementById('title').value
     let Author = document.getElementById('author').value
     let Pages = document.getElementById('pages').value
-    let checkbox = document.getElementById('checkbox')
+  
+
+
 
     //Read button code
     let buttonOfcheck = document.createElement('button')
     if(checkbox.checked == true){
-    buttonOfcheck.innerHTML='Read'}
+    buttonOfcheck.innerHTML='Readed'}
     else {
         buttonOfcheck.innerHTML ='NoReaded'
         buttonOfcheck.style.backgroundColor = 'red'
     }
-    buttonOfcheck.classList.add('buttunOfCheckbox')
+    buttonOfcheck.classList.add('buttonOfCheckbox')
   
     buttonOfcheck.addEventListener('click', ()=>{
         if(buttonOfcheck.style.backgroundColor =='red') {
                 buttonOfcheck.style.backgroundColor ='#7CE578'
-                buttonOfcheck.innerHTML ='Read'      
+                buttonOfcheck.innerHTML ='Readed'      
         } else {
             buttonOfcheck.style.backgroundColor ='red'
             buttonOfcheck.innerHTML='NoReaded'
         }
        
-        
-    })
-    let removeBtn = document.createElement('button')
-    removeBtn.innerHTML = 'Remove'
-    removeBtn.classList.add('removeBtn')
-    let newBook = new Book(Title, Author, Pages,buttonOfcheck, checkbox,removeBtn)
+})
+   
+let newBook = new Books(Title, Author, Pages, buttonOfcheck)
     myLibrary.push(newBook)
-   
     render()
-
-   removeBtn.addEventListener('click', (index) =>{
-            myLibrary.splice(index, 1)
-            removeBtn.parentElement.remove()
-
-   })
-   
-        
   
 }
 
+    
+    submitBtn.addEventListener('click', ()=>{
+        //The cards will only be created if all the inputs all correctly filled out
+        if(Title,Author,Pages.checkValidity() == true){
 
-// Taking the DOM elements
-let formContainer = document.getElementById('formContainer')
-let submitBtn = document.getElementById('submitBtn')
-let cardsSpace = document.getElementById('cards-space')
-let inputs = document.querySelectorAll('input')
-let form = document.getElementById('card')
-let buttons = document.querySelectorAll('button')
-let checkbox = document.getElementById('checkbox')
-
-
-submitBtn.addEventListener('click', ()=>{
-   
-    addBooksToLibrary()
-   
-})
-
-
-
- 
+        addBookToLibrary()
+    }
+      
+          
+    })
 
 
 
